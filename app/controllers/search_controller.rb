d0c15cf46499c12d217fe955bc88a2391a116fe9
Diana Buffone <diana.pamelaet@gmail.com>
2021-04-05 11:49:19 -0400

    Establish api connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
-  end 
+    conn = Faraday.new("https://last-airbender-api.herokuapp.com")
+    response = conn.get("/api/v1/characters?affiliation=FIRE+NATION&perPage=200")
+    @members = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
+  end
 end
