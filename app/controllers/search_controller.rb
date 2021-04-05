42f9aaa70e77ec2f3b3050dfe5b172d4e9228c98
Diana Buffone <diana.pamelaet@gmail.com>
2021-04-05 12:17:43 -0400

    Display mvp

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,5 @@ class SearchController < ApplicationController
     conn = Faraday.new("https://last-airbender-api.herokuapp.com")
     response = conn.get("/api/v1/characters?affiliation=FIRE+NATION&perPage=200")
     @members = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
   end
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,23 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Search TLA API by Nation' do
   describe "I visit the '/' as a user" do
-    it 'displays members by nation' do
+    it 'displays count of members by nation' do
       visit root_path
 
-      select 'Fire Nation', from: 'nation' #maybe fire_nation
+      select 'Fire Nation', from: 'nation'
       click_button 'Search For Members'
 
       expect(current_path).to eq(search_path)
-      # expect(page).to have_content()
+      expect(page).to have_content("Total number of people who live in the Fire Nation: #{@members.count}")
+    end
+
+    it 'displays list with detailed information for the first 25 members' do
+      visit root_path
+
+      select 'Fire Nation', from: 'nation'
+      click_button 'Search For Members'
+
+      expect(page).to have_content("Deatails for first 25 members:")
     end
   end
 end
 
 
-
-
-# Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-# And I should see a list with the detailed information for the first 25 members of the Fire Nation.
 # And for each of the members I should see:
 # - The name of the member (and their photo, if they have one)
 # - The list of allies or "None"
